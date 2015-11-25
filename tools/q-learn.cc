550bb85a325b5e875ec44e8b50b8b0235719ef6c
Akshay Agrawal <akshaykagrawal7@gmail.com>
2015-11-25 14:46:10 -0800

    q-learn compiles, initial implementation complete

--- tools/q-learn.cc
+++ tools/q-learn.cc
@@ -5,17 +5,17 @@
 #include <random>
 #include <vector>
 
-int QLearn<S>::get_action(S state) const {
+template <class S>
+int QLearn<S>::get_action(S state) {
     ++num_iters_;
     if (epsilon_distribution_(generator_) < epsilon_) {
         return action_distribution_(generator_);
     } else {
-        double curr_q = get_q(state, kMinAction)
+        double curr_q = get_q(state, kMinAction);
         double q_opt = curr_q;
-        std::vector<int> best_actions;
-        best_actions.push_back(kMinAction);
+        std::vector<int> best_actions = { kMinAction };
         for (int a = kMinAction + 1; a <= kMaxAction; ++a) {
-            curr_q = get_q(state, a)
+            curr_q = get_q(state, a);
             if (curr_q == q_opt) {
                 best_actions.push_back(a);
             } else if (curr_q > q_opt) {
@@ -27,7 +27,7 @@ int QLearn<S>::get_action(S state) const {
         if (best_actions.size() > 1) {
             std::uniform_int_distribution<int> distribution(0,
                 best_actions.size() - 1);
-            choice = distribution(generator_);
+            int choice = distribution(generator_);
             return best_actions[choice];
         } else {
             return best_actions[0];
@@ -35,32 +35,35 @@ int QLearn<S>::get_action(S state) const {
     }
 }
 
+template <class S>
 void QLearn<S>::incorporate_feedback(S state, int action,
     double reward, S state_prime) {
     double eta = get_step_size();
     double feature_scale = eta * (
         get_q(state, action) - (
             reward + discount_ * get_v_opt(state_prime)));
-    std::map<string, double> features = featurize(state, action);
+    std::map<std::string, double> features = featurize(state, action);
     for (const auto& kv : features) {
         weights_[kv.first] -= feature_scale * kv.second;
     }
 }
 
-double QLearn<S>::get_q(S state, int action) {
-    std::map<string, double> features = featurize(state, action);
+template <class S>
+double QLearn<S>::get_q(S state, int action) const {
+    std::map<std::string, double> features = featurize(state, action);
     double score = 0;
     for (const auto& kv : features) {
-        score += weights_[kv.first] * kv.second;
+        score += weights_.at(kv.first) * kv.second;
     }
     return score;
 }
 
-double QLearn<S>::get_v_opt(S state) {
-    double curr_v = get_q(state, kMinAction)
+template <class S>
+double QLearn<S>::get_v_opt(S state) const {
+    double curr_v = get_q(state, kMinAction);
     double v_opt = curr_v;
     for (int a = kMinAction + 1; a <= kMaxAction; ++a) {
-        curr_v = get_q(state, a)
+        curr_v = get_q(state, a);
         if (curr_v > v_opt) {
             v_opt = curr_v;
         }
--- tools/q-learn.h
+++ tools/q-learn.h
@@ -17,7 +17,7 @@ public:
       kMinAction(min_action),
       kMaxAction(max_action),
       epsilon_distribution_(0, 1),
-      action_distribution_(kMinAction, kMaxAction)
+      action_distribution_(kMinAction, kMaxAction),
       generator_(std::chrono::system_clock::now().time_since_epoch().count()),
       num_iters_(0) {
         assert(kMinAction <= kMaxAction); 
@@ -25,10 +25,11 @@ public:
         assert(discount >= 0 && discount <= 1);
     };
 
-    virtual int get_action(S state) const;
+    virtual int get_action(S state);
     virtual void incorporate_feedback(S state, int action,
       double reward, S state_prime);
-    virtual std::map<string, double> featurize(S state, int action) const = 0;
+    virtual std::map<std::string, double>
+        featurize(S state, int action) const = 0;
 
 private:
     // We use greedy-epsilon search, with 0 <= epsilon_ <= 1
@@ -40,15 +41,11 @@ private:
     int num_iters_;
     const int kMinAction;
     const int kMaxAction;
-    std::map<string, double> weights_;
-
-    // TODO(akshayka): Define a custom key class
-    // map state-action pairs to weights
-    std::map<std::pair<S, int>, std::vector<double>> weights_;
+    std::map<std::string, double> weights_;
 
     double get_step_size() const { return 1.0 / std::sqrt(num_iters_); }
     double get_q(S state, int action) const;
     double get_v_opt(S state) const;
-}
+};
 
 #endif
