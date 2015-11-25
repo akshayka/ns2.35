4313545f2347f6b116f3d0f6ef62b6d8e54069b7
Akshay Agrawal <akshaykagrawal7@gmail.com>
2015-11-25 12:51:23 -0800

    Module for q-learning

---
+++ tools/q-learn.cc
@@ -0,0 +1,63 @@
+#include "q-learn.h"
+
+#include <chrono>
+#include <cstdlib>
+#include <random>
+#include <vector>
+
+int QLearn<S>::get_action(S state) const {
+    ++num_iters_;
+    if (epsilon_distribution_(generator_) < epsilon_) {
+        return action_distribution_(generator_);
+    } else {
+        double curr_q = get_q(state, kMinAction)
+        double q_opt = curr_q;
+        std::vector<int> best_actions { kMinAction };
+        for (int a = kMinAction + 1; a <= kMaxAction; ++a) {
+            curr_q = get_q(state, a)
+            if (curr_q == q_opt) {
+                best_actions.push_back(a);
+            } else if (curr_q > q_opt) {
+                q_opt = curr_q;
+                best_actions.clear();
+                best_actions.push_back(a);
+            }
+        }
+        if (best_actions.size() > 1) {
+            std::uniform_int_distribution<int> distribution(0,
+                best_actions.size() - 1);
+            choice = distribution(generator_);
+            return best_actions[choice];
+        } else {
+            return best_actions[0];
+        }
+    }
+}
+
+void QLearn<S>::incorporate_feedback(S state, int action,
+    double reward, S state_prime) {
+    double eta = get_step_size();
+    double feature_scale = eta * (
+        get_q(state, action) - (
+            reward + discount_ * get_v_opt(state_prime)));
+    std::map<string, double> features = featurize(state, action);
+    for (const auto& kv : features) {
+        weights_[kv.first] -= feature_scale * kv.second;
+    }
+}
+
+double QLearn<S>::get_q(S state, int action) {
+
+}
+
+double QLearn<S>::get_v_opt(S state) {
+    double curr_v = get_q(state, kMinAction)
+    double v_opt = curr_v;
+    for (int a = kMinAction + 1; a <= kMaxAction; ++a) {
+        curr_v = get_q(state, a)
+        if (curr_v > v_opt) {
+            v_opt = curr_v;
+        }
+    }
+    return v_opt;
+}
---
+++ tools/q-learn.h
@@ -0,0 +1,53 @@
+#ifndef q_learn
+#define q_learn
+
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <map>
+#include <random>
+#include <utility>
+
+template <class S>
+class QLearn {
+public:
+    QLearn(double epsilon, double discount, int min_action, int max_action)
+    : epsilon_(epsilon),
+      discount_(discount),
+      kMinAction(min_action),
+      kMaxAction(max_action),
+      epsilon_distribution_(0, 1),
+      action_distribution_(kMinAction, kMaxAction)
+      generator_(std::chrono::system_clock::now().time_since_epoch().count()),
+      num_iters_(0) {
+        assert(kMinAction <= kMaxAction); 
+        assert(epsilon >= 0 && epsilon <= 1);
+        assert(discount >= 0 && discount <= 1);
+    };
+
+    virtual int get_action(S state) const;
+    virtual void incorporate_feedback(S state, int action,
+      double reward, S state_prime);
+    virtual std::map<string, double> featurize(S state, int action) const = 0;
+private:
+    // We use greedy-epsilon search, with 0 <= epsilon_ <= 1
+    std::uniform_int_distribution<int> action_distribution_;
+    std::uniform_real_distribution<double> epsilon_distribution_;
+    std::default_random_engine generator_;
+    double epsilon_;
+    double discount_;
+    int num_iters_;
+    const int kMinAction;
+    const int kMaxAction;
+    std::map<string, double> weights_;
+
+    // TODO(akshayka): Define a custom key class
+    // map state-action pairs to weights
+    std::map<std::pair<S, int>, std::vector<double>> weights_;
+
+    double get_step_size() const { return 1.0 / std::sqrt(num_iters_); }
+    double get_q(S state, int action) const;
+    double get_v_opt(S state) const;
+}
+
+#endif
