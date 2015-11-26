576a05625e394db97e6a71b93925aea5f8edba58
Akshay Agrawal <akshaykagrawal7@gmail.com>
2015-11-26 11:05:46 -0800

    Move template implementation to header file, update Makefile.in

--- tcp/tcp-naive-q.h
+++ tcp/tcp-naive-q.h
@@ -3,38 +3,12 @@
 
 #include <iomanip>
 #include <map>
-#include <sstream>
 #include <string>
 #include <utility>
 
 #include "ip.h"
 #include "q-learn.h"
 #include "tcp.h"
-
-class NaiveState {
-public:
-    double rtt_ratio = 1;
-    double min_rtt = -1;
-    double last_rtt = -1;
-};
-
-class NaiveQLearn : public QLearn<NaiveState> {
-public:
-    NaiveQLearn(double epsilon, double discount,
-        int min_action, int max_action) :
-        QLearn(epsilon, discount, min_action, max_action) {};
-
-    std::map<std::string, double> featurize(NaiveState state,
-        int action) const {
-        std::map<std::string, double> feature_map;
-        std::ostringstream os;
-        // Might be a good idea to discretize even more
-        os << std::fixed << std::setprecision(1) << state.rtt_ratio;
-        feature_map[os.str()] = 1;
-        feature_map[std::to_string(action)] = 1;
-        return feature_map;
-    }
-};
 
 class NaiveQTcpAgent : public virtual TcpAgent {
 private:
